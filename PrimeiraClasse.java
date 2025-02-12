27abae5693c6d244706084541e594f2d39f8fea5
FerRufato <131503375+FerRufato@users.noreply.github.com>
2025-02-11 21:36:41 -0300

    Add files via upload
    "Adicionando arquivos do módulo 5"

---
+++ PrimeiraClasse.java
@@ -0,0 +1,7 @@
+public class PrimeiraClasse {
+    public static void main(String[] args) {
+        System.out.println("Hello Fernanda Rufato");
+    }
+}
+//javac src/PrimeiraClasse.java -d bin
+//java -cp bin PrimeiraClasse
