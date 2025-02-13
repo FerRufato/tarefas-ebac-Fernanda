d8bc86bee7837ae62c2880726ac09864361232d9
Fernanda Rufato <nandaoliveira1985@terra.com.br>
2025-02-13 16:13:28 -0300

    Adicionando cálculo de média na pasta Tarefas_calculo_media

---
+++ Tarefas_calculo_media/MediaNotas.java
@@ -0,0 +1,25 @@
+public class MediaNotas {
+    public static void main(String[] args) {
+
+
+        // Definição das 4 notas manualmente
+
+        double nota1 = 7.5;
+        double nota2 = 8.0;
+        double nota3 = 6.5;
+        double nota4 = 9.0;
+
+        
+        // Cálculo da média
+
+        double media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+
+        // Impressão do resultado
+
+        System.out.println("A média das notas é: " + media);
+    }
+}
+
+
+//A média das notas é: 7.75
