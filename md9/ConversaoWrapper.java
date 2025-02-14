68b8f225cf4ac6e77bec25f6b6f3e52840a79c86
Fernanda Rufato <nandaoliveira1985@terra.com.br>
2025-02-13 22:47:17 -0300

    Adicionando exercício de casting e conversão de Wrapper

---
+++ md9/ConversaoWrapper.java
@@ -0,0 +1,9 @@
+public class ConversaoWrapper {
+    public static void main(String[] args) {
+        int valorPrimitivo = 10; 
+        Integer valorWrapper = Integer.valueOf(valorPrimitivo); 
+
+        System.out.println(valorPrimitivo);
+        System.out.println( valorWrapper);
+    }
+}
