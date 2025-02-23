da151a7bdda9ca9178e3305c2ec4edc47b665943
Fernanda Rufato <nandaoliveira1985@terra.com.br>
2025-02-22 23:52:45 -0300

    resolvendo conflitos

---
+++ tarefa-md13/Pessoa.java
@@ -0,0 +1,40 @@
+public abstract class Pessoa {
+    private String nome;
+    private String endereco;
+    private String telefone;
+
+    // Construtor
+    public Pessoa(String nome, String endereco, String telefone) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
+
+    // Getters e Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    // Método abstrato que será implementado nas subclasses
+    public abstract void exibirInformacoes();
+}
