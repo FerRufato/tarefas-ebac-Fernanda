023a992e693652016c52e4757a74e578ae70cd94
Fernanda Rufato <nandaoliveira1985@terra.com.br>
2025-02-22 23:34:49 -0300

    first commit

---
+++ PessoaJuridica.java
@@ -0,0 +1,41 @@
+
+
+public class PessoaJuridica extends Pessoa {
+    private String cnpj;
+    private String razaoSocial;
+
+    
+    public PessoaJuridica(String nome, String endereco, String telefone, String cnpj, String razaoSocial) {
+        super(nome, endereco, telefone);
+        this.cnpj = cnpj;
+        this.razaoSocial = razaoSocial;
+    }
+
+   
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+    
+    @Override
+    public void exibirInformacoes() {
+        System.out.println("Pessoa Jurídica: ");
+        System.out.println("Nome: " + getNome());
+        System.out.println("Endereço: " + getEndereco());
+        System.out.println("Telefone: " + getTelefone());
+        System.out.println("CNPJ: " + getCnpj());
+        System.out.println("Razão Social: " + getRazaoSocial());
+    }
+}
---
+++ Teste.java
@@ -0,0 +1,14 @@
+
+
+public class Teste {
+    public static void main(String[] args) {
+       
+        PessoaFisica pf = new PessoaFisica("João Silva", "Rua X, 123", "1234-5678", "123.456.789-00", "01/01/1980");
+        PessoaJuridica pj = new PessoaJuridica("Empresa X", "Av. Y, 456", "9876-5432", "12.345.678/0001-99", "Empresa X LTDA");
+
+       
+        pf.exibirInformacoes();
+        System.out.println();
+        pj.exibirInformacoes();
+    }
+}
